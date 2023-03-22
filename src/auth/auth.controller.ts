57ccd5e92213c00885d7b3872f73bd6dd0cc5cd5
Rebecca <beca.dance@gmail.com>
2023-03-22 10:07:01 -0400

    basic post requests

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,18 @@
+import { Controller } from '@nestjs/common';
+import { Post } from '@nestjs/common/decorators';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('signup')
+  signup() {
+    return 'I am signed up';
+  }
+
+  @Post('signin')
+  signin() {
+    return 'I am signed in';
+  }
+}
