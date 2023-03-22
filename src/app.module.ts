244e0f4b4d52cc6290746595777b72be1999f773
Rebecca <beca.dance@gmail.com>
2023-03-22 09:17:02 -0400

    3 modules created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
+//main module - like app.js
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
+import { AuthModule } from './auth/auth.module';
 
+//a decorator, adding metadata to the module we are coding
+//module organize your app, declared with a class and annotated by a decorated module
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UserModule, BookmarkModule, AuthModule],
 })
 export class AppModule {}
